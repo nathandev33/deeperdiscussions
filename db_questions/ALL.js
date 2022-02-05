07c2cef63931820aedd95b64a5e54bdde009618c
Nathan Dev <nathandev33@gmail.com>
2022-02-05 18:06:26 +0100

    zkouším languages

--- db_questions/ALL.js
+++ db_questions/ALL.js
@@ -1,17 +1,4 @@
-// SELECTED ELEMENTS
-const toggleBtn = document.getElementById("toggle-btn");
-const sidebar = document.getElementById("sidebar");
-const links = document.querySelectorAll(".single-link");
-const cardText = document.querySelector(".card-text");
-const nextBtn = document.querySelector(".next-btn");
-const shuffle = document.querySelector(".shuffle");
-const card = document.querySelector(".card");
-const cardInfo = document.querySelector(".card-number");
-const saveSettings = document.querySelector(".save-settings");
-const checkboxes = document.querySelectorAll("input[type=checkbox]");
-const previousCardBtn = document.querySelector(".previous-card-btn");
-
-let questions = [
+const questions = [
   {
     category: "",
     id: 0,
@@ -1501,7 +1488,7 @@ let questions = [
   },
 ];
 
-let poznavaci_ = [
+const poznavaci_ = [
   {
     category: "",
     id: 0,
@@ -2991,7 +2978,7 @@ let poznavaci_ = [
   },
 ];
 
-let hypoteticke_ = [
+const hypoteticke_ = [
   {
     category: "",
     id: 0,
@@ -3341,7 +3328,7 @@ let hypoteticke_ = [
   },
 ];
 
-let crazy_ = [
+const crazy_ = [
   {
     category: "",
     id: 0,
@@ -3852,238 +3839,4 @@ let crazy_ = [
   },
 ];
 
-// POLE
-//questions a poznavaci_ jsou duplikáty, když budu přidávat otázky, musím do obou, jinak nebudu mít aktuální!
-
-let language_;
-// language = cs;
-
-let URLlanguage;
-window.addEventListener("DOMContentLoaded", function () {
-  const URLlanguage = window.location.pathname;
-  console.log("current url is" + URLlanguage);
-  switch (URLlanguage) {
-    case "/cs.html":
-      // language = cs;
-      language_ = "cs";
-      console.log("čeština");
-      break;
-    case "/index.html":
-      // language = en;
-      language_ = "en";
-      console.log("angličtina");
-      break;
-    default:
-    // language = en;
-  }
-
-  // console.log(questions[2].text);
-  let indexRandomCisla = 0;
-
-  numberInfo();
-  const select = document.getElementById("select-language");
-  let selectedLanguage = select.value;
-  console.log(selectedLanguage);
-  // proměnná language určuje do jaké databáze otázek se budu dívat
-
-  // select.addEventListener("change", function handleChange(event) {
-  //   console.log(event.target.value);
-  //   //podle toho, jaký jazyk vyberu, do proměnné language se uloží příslušný jazyk (klíčové pro rozhodnutí, do jaké databáze se budu dívat)
-  //   selectedLanguage = event.target.value;
-
-  //   switch (selectedLanguage) {
-  //     case "en":
-  //       // language = en;
-  //       window.location.href = "https://deeperdiscussions.com/";
-  //       break;
-
-  //     case "cs":
-  //       // language = cs;
-  //       window.location.href = "https://deeperdiscussions.com/cs.html";
-  //       break;
-  //     default:
-  //       // language = en;
-  //       window.location.href = "https://deeperdiscussions.com/";
-  //   }
-  //   sidebar.classList.remove("show-sidebar");
-  //   shuffle.click(); // po změně jazyku zamíchám balíček
-  // });
-
-  let vygenerovanaRandomCisla = myRandomInts(
-    questions.length - 1,
-    questions.length - 1
-  );
-  // console.log("vygenerovanaRandomCisla:", vygenerovanaRandomCisla);
-  // console.log("delka otazek", questions.length);
-
-  // NAV - SIDEBAR
-  toggleBtn.addEventListener("click", () => {
-    sidebar.classList.toggle("show-sidebar");
-  });
-
-  const closeSidebar = document.querySelector(".close-sidebar");
-  closeSidebar.addEventListener("click", () => {
-    sidebar.classList.remove("show-sidebar");
-  });
-
-  links.forEach((link) => {
-    link.addEventListener("click", () => {
-      sidebar.classList.toggle("show-sidebar");
-    });
-  });
-
-  nextBtn.addEventListener("click", () => {
-    let questionIndex = vygenerovanaRandomCisla[indexRandomCisla];
-    if (indexRandomCisla < questions.length) {
-      cardText.textContent = questions[questionIndex][`${language_}`];
-      indexRandomCisla++;
-      // console.log("číslo je: " + indexRandomCisla);
-      console.log(questions[questionIndex][`${language_}`]);
-      console.log(questionIndex);
-      console.log(questions[questionIndex].cs);
-    } else {
-      cardText.textContent =
-        "THERE ARE NO NEW QUESTIONS TO DISPLAY IN THIS PACKAGE.";
-    }
-    numberInfo();
-  });
-
-  //previous card - button
-  previousCardBtn.addEventListener("click", function () {
-    if (indexRandomCisla > 0) {
-      indexRandomCisla--;
-      questionIndex = vygenerovanaRandomCisla[indexRandomCisla - 1];
-      numberInfo();
-      // console.log("index je: " + questionIndex);
-      cardText.textContent = questions[questionIndex][`${language_}`];
-      // console.log("číslo je: " + indexRandomCisla);
-    } else {
-      cardText.textContent =
-        "YOU'RE AT THE VERY BEGINNING, DRAW THE FIRST QUESTION.";
-      indexRandomCisla == 0;
-    }
-  });
-
-  //shuffle button
-  shuffle.addEventListener("click", () => {
-    indexRandomCisla = 0;
-    vygenerovanaRandomCisla = myRandomInts(
-      questions.length - 1,
-      questions.length - 1
-    );
-    numberInfo();
-
-    // hourglass
-    cardText.innerHTML = `
-<article class="hourglass-container">          
-        <div class="hourglass"> 
-        </div>
-    </article>
-`;
-    setTimeout(() => {
-      cardText.innerHTML = `<span style="font-size:8rem;">?</span>`;
-    }, 2100);
-  });
-
-  // FUNKCE
-  function numberInfo() {
-    cardInfo.innerHTML = `${indexRandomCisla} / ${questions.length - 1}`;
-  }
-
-  // vygeneruje arrae randoms - 121 random čísel od 1-121
-  function myRandomInts(quantity, max) {
-    const set = new Set();
-    while (set.size < quantity) {
-      set.add(Math.floor(Math.random() * max)); // bylo původně ...."* max) + 1))
-    }
-    const randoms = Array.from(set);
-
-    return randoms;
-  }
-
-  saveSettings.addEventListener("click", checkCheckbox);
-
-  function checkCheckbox() {
-    var poznavaciCheckbox = document.querySelector(".poznavaci");
-    var hypotetickeCheckbox = document.querySelector(".hypoteticke");
-    var crazyCheckbox = document.querySelector(".crazy");
-
-    if (
-      poznavaciCheckbox.checked == false &&
-      hypotetickeCheckbox.checked == false &&
-      crazyCheckbox.checked == false
-    ) {
-      questions = [];
-      cardText.textContent = "Select at least 1 question type in the settings.";
-    } else if (
-      poznavaciCheckbox.checked == false &&
-      hypotetickeCheckbox.checked == false &&
-      crazyCheckbox.checked == true
-    ) {
-      questions = crazy_;
-    } else if (
-      poznavaciCheckbox.checked == false &&
-      hypotetickeCheckbox.checked == true &&
-      crazyCheckbox.checked == false
-    ) {
-      questions = hypoteticke_;
-    } else if (
-      poznavaciCheckbox.checked == false &&
-      hypotetickeCheckbox.checked == true &&
-      crazyCheckbox.checked == true
-    ) {
-      questions = [...hypoteticke_, ...crazy_];
-    } else if (
-      poznavaciCheckbox.checked == true &&
-      hypotetickeCheckbox.checked == false &&
-      crazyCheckbox.checked == false
-    ) {
-      questions = poznavaci_;
-    } else if (
-      poznavaciCheckbox.checked == true &&
-      hypotetickeCheckbox.checked == false &&
-      crazyCheckbox.checked == true
-    ) {
-      questions = [...poznavaci_, ...crazy_];
-    } else if (
-      poznavaciCheckbox.checked == true &&
-      hypotetickeCheckbox.checked == true &&
-      crazyCheckbox.checked == false
-    ) {
-      questions = [...poznavaci_, ...hypoteticke_];
-    } else {
-      questions = [...poznavaci_, ...hypoteticke_, ...crazy_];
-    }
-
-    vygenerovanaRandomCisla = myRandomInts(
-      questions.length - 1,
-      questions.length
-    );
-    numberInfo();
-
-    // ocheckuje, jestli tam jsou duplikáty. Pokud jo, vypíše je, pokud ne, returne prázdné erej
-    // const lookup = questions.reduce((a, e) => {
-    //   a[e.text] = ++a[e.text] || 0;
-    //   return a;
-    // }, {});
-    // console.log(questions.filter(e => lookup[e.text]));
-    return questions;
-  }
-
-  window.addEventListener("load", function () {
-    // add an event listener for the change event
-    const radioButtons = document.querySelectorAll('input[name="sortType"]');
-    for (const radioButton of radioButtons) {
-      radioButton.addEventListener("change", showSelected);
-    }
-
-    function showSelected(e) {
-      console.log(e);
-      if (this.checked) {
-        document.querySelector(
-          "#output"
-        ).innerText = `You selected ${this.value}`;
-      }
-    }
-  });
-});
+export { questions, poznavaci_, hypoteticke_, crazy_ };
